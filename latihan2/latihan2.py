54595a6e96e7954e04fc3f0ad307f4a0a00443fc
Mohamad Galih Permana <galihpriv99@gmail.com>
2019-02-15 20:05:30 -0500

    Flowchart program menampilkan bilangan terbesar

---
+++ latihan2/latihan2.py
@@ -0,0 +1,15 @@
+print ('|-------------------------------------|')
+print ('|LATIHAN 2|')
+print ('|-------------------------------------|')
+print ('')
+
+a=1
+max=0
+while a!=0:
+    if a>max:
+        max=a
+    a=int(input('Masukkan bilangan :'))
+    if a==0:
+         break
+        
+print ('Nilai terbesarya adalah :',max)
